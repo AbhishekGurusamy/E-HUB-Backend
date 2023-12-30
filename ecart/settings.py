c2e19004fa6ad3dc3fd9b18e41b64e12a43fdf5e
Abhishek G <abhiguru.inf@gmail.com>
2023-12-30 17:24:45 +0530

    db setup

--- ecart/settings.py
+++ ecart/settings.py
@@ -27,19 +27,27 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:4200",
+]
+
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'upload'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'ecart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class StoreUploadImg(models.Model):
+    userID = models.IntegerField(max_length = 20)
+    imgURL = models.CharField()
